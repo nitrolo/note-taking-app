5714d66faa647043376a23a44f3e9c07c738aee1
“nitrolo” <nitrolo225@gmail.com>
2021-07-28 19:11:12 +0530

    added appropriate ux for save button

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 
 const AddNote = ({ handleAddNote }) => {
   const [noteText, setNoteText] = useState('');
+
   const handleChange = (event) => {
     setNoteText(event.target.value);
   };
+
   const handleSaveClick = (event) => {
-    handleAddNote(noteText);
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText('');
+    }
   };
+
   return (
     <div className='note new'>
       <textarea
